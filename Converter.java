9a9f1635e867f52a4857868afa9bfa6fd6d03c27
OlegT <o.o.to@yandex.ru>
2023-08-31 16:27:11 +0700

    declared main and class

---
+++ Converter.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            // здесь нужно вывести результат
+        }
+    }
